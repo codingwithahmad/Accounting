d45debc7a078cfa798693e15a19842a062cb1294
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-08 18:44:55 +0330

    just add a simple Sabt model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sabt(models.Model):
+    title = models.CharField(max_length=128)
+    inconme = models.IntegerField()
+    spending = models.IntegerField()
