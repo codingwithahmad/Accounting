436f50b2ead576bc72ddc75a1276e7a325ec4e85
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-09 15:23:30 +0330

    add media url and media root to settings and append static function to config/urls.urlpatterns to read media file from server

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+
+# Media files and folder
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 AUTH_USER_MODEL = 'user.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
