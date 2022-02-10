4a02593ea9b00370db902c96b197d1009074cd81
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-10 17:34:14 +0330

    fix income field typo in Sabt model then migrate to db

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Sabt(models.Model):
     title = models.CharField(max_length=128)
-    inconme = models.IntegerField()
+    income = models.IntegerField()
     spending = models.IntegerField()
