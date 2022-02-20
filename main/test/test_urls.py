79a2b8110934e73d6d1a673d6f92a46c5404c8d6
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-20 14:34:40 +0330

    add test_AllSabt_url_view to test view for AllSabt url

--- main/test/test_urls.py
+++ main/test/test_urls.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
-from django.urls import reverse
+from django.urls import reverse, resolve
+from main.views import AllSabt
 
 
 class TestUrls(TestCase):
@@ -11,3 +12,7 @@ class TestUrls(TestCase):
     def test_AllSabt_url(self):
         response = self.client.get(reverse("main:AllSabt"))
         self.assertEqual(response.status_code, 200)
+
+    def test_AllSabt_url_view(self):
+        all_sabt = reverse('main:AllSabt')
+        self.assertEqual(resolve(all_sabt).func.view_class, AllSabt)
