e4be3ffc7f5388bbd498353e04e51b9a2d210c62
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-21 15:09:17 +0330

    add url and view test for CreateSabt url in main/test_urls.py

--- main/test/test_urls.py
+++ main/test/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse, resolve
-from main.views import AllSabt
+from main.views import AllSabt, CreateSabt
 
 
 class TestUrls(TestCase):
@@ -16,3 +16,11 @@ class TestUrls(TestCase):
     def test_AllSabt_url_view(self):
         all_sabt = reverse('main:AllSabt')
         self.assertEqual(resolve(all_sabt).func.view_class, AllSabt)
+
+    def test_CreateSabt_url(self):
+        response = self.client.get(reverse("main:CreateSabt"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_CreateSabt_url_view(self):
+        url = reverse("main:CreateSabt")
+        self.assertEqual(resolve(url).func.view_class, CreateSabt)
