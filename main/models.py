a70d3b5c18f7cf2c20c96cdd39d111b83928c276
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-23 15:37:49 +0330

    create Category model and add category field to Sabt model then migrate to db

--- main/models.py
+++ main/models.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True,
+                               on_delete=models.SET_NULL,
+                               related_name='children')
+    title = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Sabt(models.Model):
     title = models.CharField(max_length=128)
     income = models.IntegerField()
     spending = models.IntegerField()
+    category = models.ManyToManyField(Category, related_name="sabts")
     date = models.DateTimeField(default=timezone.now)
-
 
     def __str__(self):
         return self.title
