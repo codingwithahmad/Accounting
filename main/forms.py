0c5c68845193bb0310ad38a7f8ebeb8c5481728e
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-24 15:54:20 +0330

    make cates field to SabtFrom who is a Chackbox field and add to class Meta fields

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from .models import Sabt
+from .models import Sabt, Category
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 
 class SabtForm(forms.ModelForm):
-
+    cats = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple, required=True)
 
     class Meta:
         model = Sabt
-        fields = ['title', 'income', 'spending', 'date']
+        fields = ['title', 'income', 'spending', 'date', 'cats']
         widgets= {
             'date': DateTimePickerInput(),
         }
